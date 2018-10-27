984e0ac15c0e2193381664f87fcbde924ea35037
kvsrinivas91 <43247128+kvsrinivas91@users.noreply.github.com>
2018-10-27 09:27:57 +0530

    Update for code

--- DiscountNotification/PriceReader.py
+++ DiscountNotification/PriceReader.py
@@ -5,7 +5,7 @@ def removeComma(s):
 	s = s.split(",")
 	return "".join(s)
 
-x = urllib.request.urlopen('https://www.amazon.in/dp/B07GVMTWWX/ref=cm_sw_r_cp_ep_dp_YUKWBbV1ZQ1KW')
+x = urllib.request.urlopen('https://www.amazon.in/Apple-iPhone-Silver-64GB-Storage/dp/B0711T2L8K/ref=sr_1_2?s=electronics&ie=UTF8&qid=1540612639&sr=1-2&keywords=iphone')
 soup =BeautifulSoup(x,'html.parser')
 a = soup.find('span',id='priceblock_dealprice')
 price=a.text
